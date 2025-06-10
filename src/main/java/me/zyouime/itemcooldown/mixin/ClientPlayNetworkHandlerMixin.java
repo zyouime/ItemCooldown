d0b94f6cbccc509b705714f1e332aa478d7b0153
zyouime <igorneizvestny69@gmail.com>
2025-06-11 01:09:13 +0300

    cooldown fix

--- src/main/java/me/zyouime/itemcooldown/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/zyouime/itemcooldown/mixin/ClientPlayNetworkHandlerMixin.java
@@ -12,8 +12,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.packet.s2c.play.CooldownUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,6 +25,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin {
 
+    @Shadow @Final private MinecraftClient client;
+
     @Inject(method = "onCooldownUpdate", at = @At("RETURN"))
     private void onCooldownUpdate(CooldownUpdateS2CPacket packet, CallbackInfo ci) {
         int cooldown = packet.getCooldown();
@@ -29,12 +34,8 @@ public abstract class ClientPlayNetworkHandlerMixin {
         if (cooldown > 0) {
             ItemCooldown.Settings settings = ItemCooldown.getInstance().settings;
             settings.items.getValue().get(settings.selectedCategory.getValue()).forEach(ct -> {
-                if (ct instanceof VanillaItemCooldown vic) {
-                    if (vic.getItem().getItem().equals(item)) {
-                        if (vic.getCooldown() < cooldown) {
-                            vic.setCooldown(cooldown);
-                        }
-                    }
+                if (ct instanceof VanillaItemCooldown vic && vic.getItem().getItem().equals(item) && vic.getCooldown() < cooldown) {
+                    vic.setCooldown(cooldown);
                 }
             });
         }
@@ -42,7 +43,7 @@ public abstract class ClientPlayNetworkHandlerMixin {
 
     @Inject(method = "onScreenHandlerSlotUpdate", at = @At("RETURN"))
     private void onScreenHandlerSlotUpdate(ScreenHandlerSlotUpdateS2CPacket packet, CallbackInfo ci) {
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        ClientPlayerEntity player = this.client.player;
         if (player == null) {
             return;
         }
@@ -79,6 +80,13 @@ public abstract class ClientPlayNetworkHandlerMixin {
                 CooldownManager.setCooldownIfNeeded(usedItem);
             }
             clientPlayer.clearItem();
+        }
+    }
+
+    @Inject(method = "onHealthUpdate", at = @At("HEAD"))
+    private void onHealthUpdate(HealthUpdateS2CPacket packet, CallbackInfo ci) {
+        if (packet.getHealth() <= 0.0f) {
+            ((UseItem) this.client.player).clearItem();
         }
     }
 
